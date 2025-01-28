65a6f0ce26d80237e6b9fbec7dcbd1eafe304d92
krianlloydl <krianlloydl@gmail.com>
2025-01-29 00:16:17 +0800

    Adjust navlink position

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -44,7 +44,7 @@ const Navbar = () => {
           className="hidden w-full md:block md:w-auto"
           id="navbar-multi-level"
         >
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg md:space-x-10 mr-20 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
             {user.id !== null ? (
               user.isAdmin ? (
                 <>
